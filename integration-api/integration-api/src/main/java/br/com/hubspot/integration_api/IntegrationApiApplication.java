bf3846d7b742e04e0cd52625ba4e658ae0b714c2
Yuri Padlipskas <yupadlipskas@gmail.com>
2025-04-04 18:49:35 -0300

    skeleton

---
+++ integration-api/integration-api/src/main/java/br/com/hubspot/integration_api/IntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hubspot.integration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationApiApplication.class, args);
+	}
+
+}
